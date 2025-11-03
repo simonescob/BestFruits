220c5800a70ed6a98110e5433dea126f99d7c484
simon <snsimonescobar@gmail.com>
2025-11-02 21:31:21 -0500

    Initial Commit

---
+++ BestFruits/BestFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestFruitsApp.swift
+//  BestFruits
+//
+//  Created by Simon on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BestFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestFruitsTests/BestFruitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BestFruitsTests.swift
+//  BestFruitsTests
+//
+//  Created by Simon on 11/2/25.
+//
+
+import Testing
+@testable import BestFruits
+
+struct BestFruitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
